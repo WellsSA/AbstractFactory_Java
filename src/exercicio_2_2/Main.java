275c2f627eb6ebb77717223b04ef961c5b671ce5
WellsSA <wel.cavzod@gmail.com>
2020-06-07 18:31:29 -0300

    feat(): exercicio_2_2 final adjusts

--- src/exercicio_2_2/Main.java
+++ src/exercicio_2_2/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		System.out.println(args.length);
-		String password = "desi gnpatt er ns2";
+		String password = "desi gnpatt er ns";
 
 		int kind = FileReadFactory.PUBLIC;
 
