889e4bfa491866a416e0ea725c40f02f40a3bcf2
WellsSA <wel.cavzod@gmail.com>
2020-06-07 19:13:52 -0300

    chore(): exercicio_4_1 logic update

--- src/exercicio_4_1/Main.java
+++ src/exercicio_4_1/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		String[][] vetor = {{"1","2","3","4"}, {"A","Zezim","Zuleide", "Creide"}};
 		/*
 		 * expected: 
 		 * [
@@ -14,12 +15,15 @@ public class Main {
 		 *  [coluna1, coluna2, coluna3]
 		 * ]
 		 */
-
-		Map<Integer, String> mapaNomes = new HashMap<Integer, String>(); 
 		
-	    mapaNomes.put(1, "João Delfino");
-	    mapaNomes.put(2, "Maria do Carmo");
-	    mapaNomes.put(3, "Claudinei Silva");
+		Map<Integer, String> mapaNomes = new HashMap<Integer, String>();
+		
+		
+		for(int i = 0; i < vetor[0].length; i++) {
+			mapaNomes.put(Integer.parseInt(vetor[0][i]), vetor[1][i]);
+		}
+		
+		
 	    
 	    System.out.println(mapaNomes);
 	}
