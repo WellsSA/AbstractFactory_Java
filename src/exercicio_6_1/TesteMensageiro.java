df3464e9f56cbda107bb85ea05ffc167b762d5d3
WellsSA <wel.cavzod@gmail.com>
2020-06-07 19:43:52 -0300

    feat(): exercicio_6_1 resolution

---
+++ src/exercicio_6_1/TesteMensageiro.java
@@ -0,0 +1,11 @@
+package exercicio_6_1;
+
+public class TesteMensageiro {
+
+	public static void main(String[] args) {
+		Mensageiro mensageiro = new Mensageiro();
+		
+		mensageiro.enviarMensagem();
+	}
+
+}
