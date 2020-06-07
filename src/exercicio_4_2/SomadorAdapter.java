37bf2b23431a1742d57b15cf19ef75688d9e2bc2
WellsSA <wel.cavzod@gmail.com>
2020-06-07 18:54:22 -0300

    feat(): adding somaInteiros method to addapter

--- src/exercicio_4_2/SomadorAdapter.java
+++ src/exercicio_4_2/SomadorAdapter.java
@@ -7,11 +7,22 @@ public class SomadorAdapter extends SomadorExistente implements SomadorEsperado 
 	@Override
 	public int somaVetor(int[] vetor) {
 		ArrayList<Integer> lista = new ArrayList<>();
-		
-		for(int i = 0; i < vetor.length; i++) {
+
+		for (int i = 0; i < vetor.length; i++) {
 			lista.add(vetor[i]);
 		}
-		
+
+		int resultado = somaLista(lista);
+		return resultado;
+	}
+
+	@Override
+	public int somaInteiros(int a, int b) {
+		ArrayList<Integer> lista = new ArrayList<>();
+
+		lista.add(a);
+		lista.add(b);
+
 		int resultado = somaLista(lista);
 		return resultado;
 	}
--- src/exercicio_4_2/SomadorEsperado.java
+++ src/exercicio_4_2/SomadorEsperado.java
@@ -2,4 +2,5 @@ package exercicio_4_2;
 
 public interface SomadorEsperado {
 	int somaVetor(int[] vetor);
+	int somaInteiros(int a, int b);
 }
