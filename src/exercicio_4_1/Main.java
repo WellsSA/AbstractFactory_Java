ae3284e29eb7dc2cf8579ab15e6fbeaad85ebc33
WellsSA <wel.cavzod@gmail.com>
2020-06-07 19:25:12 -0300

    feat(): implementing MapAdapter

--- src/exercicio_4_1/Main.java
+++ src/exercicio_4_1/Main.java
@@ -7,25 +7,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		String[][] vetor = {{"1","2","3","4"}, {"A","Zezim","Zuleide", "Creide"}};
-		/*
-		 * expected: 
-		 * [
-		 *  [chave1, chave2, chave3],
-		 *  [coluna1, coluna2, coluna3]
-		 * ]
-		 */
+		String[][] vet = {{"1","2","3","4"}, {"A","Zezim","Zuleide", "Creide2"}};
 		
-		Map<Integer, String> mapaNomes = new HashMap<Integer, String>();
+		Map<Integer, String> map = new MapAdapter(vet);
 		
-		
-		for(int i = 0; i < vetor[0].length; i++) {
-			mapaNomes.put(Integer.parseInt(vetor[0][i]), vetor[1][i]);
-		}
-		
-		
-	    
-	    System.out.println(mapaNomes);
+	    System.out.println(map);
 	}
 
 }
--- src/exercicio_4_1/MapAdapter.java
+++ src/exercicio_4_1/MapAdapter.java
@@ -2,10 +2,12 @@ package exercicio_4_1;
 
 import java.util.HashMap;
 
-public class MapAdapter<T, K> extends HashMap<T, K> {
+public class MapAdapter extends HashMap<Integer, String> {
 	private static final long serialVersionUID = 1L;
 
-	public MapAdapter(Integer[] x) {
-		
+	public MapAdapter(String[][] vet) {
+		for (int i = 0; i < vet[0].length; i++) {
+			this.put(Integer.parseInt(vet[0][i]), vet[1][i]);
+		}
 	}
 }
