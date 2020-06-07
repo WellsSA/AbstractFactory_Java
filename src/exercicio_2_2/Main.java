4bbecef0cbee75492453abff1bac34a6ff78a798
WellsSA <wel.cavzod@gmail.com>
2020-06-07 18:18:36 -0300

    feat(): passwrod verification

--- src/exercicio_2_2/Main.java
+++ src/exercicio_2_2/Main.java
@@ -4,16 +4,13 @@ public class Main {
 	public static void main(String[] args) {
 
 		System.out.println(args.length);
-
-		/*
-		 * String password = "desi gnpatt er ns"; int kind = publico.txt;
-		 *
-		 * if(args.length != 0 && args[0].equals(password)) { int kind =
-		 * confidencial.txt; }
-		 * 
-		 */
+		String password = "desi gnpatt er ns";
 
 		int kind = FileReadFactory.PUBLIC;
+
+		if (args.length != 0 && args[0].equals(password)) {
+			kind = FileReadFactory.CONFIDENTIAL;
+		}
 
 		FileRead fileRead = (new FileReadFactory()).getFileReadInstance(kind);
 
