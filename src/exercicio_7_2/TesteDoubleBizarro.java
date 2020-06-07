afb24d980dbcbe8323d6a339e62cf2e21b21f349
WellsSA <wel.cavzod@gmail.com>
2020-06-07 20:41:18 -0300

    feat(): creating main for DoubleBizarro

--- src/exercicio_7_2/TesteDoubleBizarro.java
+++ src/exercicio_7_2/TesteDoubleBizarro.java
@@ -1,5 +1,18 @@
 package exercicio_7_2;
 
-public class TesteDoubleBizarro {
+import java.util.Arrays;
 
+public class TesteDoubleBizarro {
+	public static void main(String[] args) {
+		DoubleBizarro[] db = new DoubleBizarro[10];
+		
+		for(int i = 0; i < 10; i++) {
+			db[i] = new DoubleBizarro(Math.floor(10*Math.random()) + Math.random());
+			System.out.println(db[i].doubleValue());
+		}
+		
+		Arrays.sort(db);
+		System.out.println("Ordenando somente pelas casas decimais");
+		System.out.println(Arrays.toString(db));
+	}
 }
