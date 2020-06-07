42c68aab32bd07caa7fb8b041fee296767162979
WellsSA <wel.cavzod@gmail.com>
2020-06-07 20:21:13 -0300

    feat(): logging sort execution time

--- src/exercicio_6_2/Main.java
+++ src/exercicio_6_2/Main.java
@@ -6,19 +6,51 @@ public class Main {
 		int[] v = { 1, 2, 34, 5, 5, 656, 7, 67, 6, 78, 87, 8, 490, 303, 4942, 34, 4, 5, 5, 46 };
 
 		SortMethod method;
+		long startTime = System.nanoTime();
 		
-		method = new BubbleSort();
+		/********************* BubbleSort **************************/
+		startTime = System.nanoTime();
+		
+		method = new BubbleSort();		
 		System.out.println("BubbleSort: " + print(method.sort(v)));
+		
+		System.out.println("Time: " + (System.nanoTime() - startTime));
+		/************************************************************/
+		
+		/********************* InsertionSort **************************/
+		startTime = System.nanoTime();
 		
 		method = new InsertionSort();
 		System.out.println("InsertionSort: " + print(method.sort(v)));
 		
+		System.out.println("Time: " + (System.nanoTime() - startTime));
+		/************************************************************/
+		
+		/********************* SelectionSort **************************/
+		startTime = System.nanoTime();
+		
 		method = new SelectionSort();
 		System.out.println("SelectionSort: " + print(method.sort(v)));
 		
+		System.out.println("Time: " + (System.nanoTime() - startTime));
+		/************************************************************/
+		
+		/********************* QuickSort **************************/
+		startTime = System.nanoTime();
+		
 		method = new QuickSort();
 		System.out.println("QuickSort: " + print(method.sort(v)));
-
+		
+		System.out.println("Time: " + (System.nanoTime() - startTime));
+		/************************************************************/
+		
+		
+		
+	
+		
+		
+		
+		
 	}
 
 	public static String print(int[] v) {
